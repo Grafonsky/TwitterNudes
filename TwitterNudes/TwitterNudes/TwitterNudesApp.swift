b5126f8c3f805dba5b6e17d228fe245fc7403121
Bohdan Hawrylyshyn <bohdanhawrylyshyn@Bohdans-MacBook-Pro.local>
2023-01-02 17:08:34 +0400

    Init

---
+++ TwitterNudes/TwitterNudes/TwitterNudesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterNudesApp.swift
+//  TwitterNudes
+//
+//  Created by Bohdan Hawrylyshyn on 02.01.23.
+//
+
+import SwiftUI
+
+@main
+struct TwitterNudesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
